b496fbb56275ccdb9c7067ab0a4be49db84dcacd
santosemerson17-rgb <santos.emerson17@gmail.com>
2026-03-11 10:06:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,24 @@
+const CACHE_NAME = "peso-app-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("App instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
